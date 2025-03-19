192b90da359bb5df76f3451d820876d82f8047fd
Tony Chen <114113204+TonyChen-26@users.noreply.github.com>
2025-03-19 11:13:59 -0400

    Delete JayPilon-g3-vlsi407-poste-e814235e66d7 directory

--- JayPilon-g3-vlsi407-poste-e814235e66d7/include/traitementQR.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-///@brief Filters the text read from the QR code by removing all characters except alphanumeric ones or spaces. It also transforms all upper case characters to lower case.
-///@param input QR code text
-///@returns The filtered text
-std::string FilterText(const std::string input);
